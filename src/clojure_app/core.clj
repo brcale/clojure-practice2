317bfbe4e70bf3d3ad918c49d3e33a27fcaf9ad4
Bruno Čale <50056973+brcale@users.noreply.github.com>
2020-10-10 14:58:03 +0200

    Playing with functions

--- src/clojure_app/core.clj
+++ src/clojure_app/core.clj
@@ -1,26 +1,38 @@
 (ns clojure-app.core
   (:gen-class))
 
-(defn -main []
-  (def data [{:name "Bruno" :age 22 :city "Zagreb" :gender "Male"}
+(def data [{:name "Bruno" :age 22 :city "Zagreb" :gender "Male"}
              {:name "Marko" :age 26 :city "Osijek" :gender "Male"}
              {:name "Marta" :age 28 :city "Zadar" :gender "Female"}
              {:name "Matej" :age 21 :city "Rijeka" :gender "Male"}
-             {:name "Ivana" :age 24 :city "Zagreb" :gender "Female"} 
+             {:name "Ivana" :age 24 :city "Zagreb" :gender "Female"}
+             {:name "Tin" :age 25 :city "Split" :gender "Male"}
+             {:name "Karlo" :age 29 :city "Zagreb" :gender "Female"} 
              ])
     (println "All data: " data)
     (defn get-names []
-      (let [names (map (fn [item] (:name item)) data)] names)
+      (let [names (mapv (fn [item] (:name item)) data)] names)
       )
       (println "Only names: " (get-names))
-      ;;not working
-      (defn is-age? [age number] 
-        (if(< num age) true false))
+      ;;filters peoples age
+      (defn filter-age []
+        (let [filter-data (filterv (fn [item]
+         (< (:age item) 25)) data)]
+          filter-data
+          )
+        )
+      ;;returns citys of people younger than 25
+      (defn filter-city [data]
+         (let [filter-data (mapv (fn [item]
+          (println (:city item))) data)]  
+      )
+      )
+    ;;returns number of males
+    (defn filter-gender-counter []
+      (let [filter-data (filterv (fn [item]
+        (= (:gender item) "Male")) data)]
+        (count filter-data) 
+          )
+        )
 
-      (defn filterAge []
-        (let [personAge (map (fn [item] (:age item)) data)] personAge)
-          (if (is-age? item 25) "Return users name" "No people younger than 25")
-        )
-      (filterAge)
-        )
   
